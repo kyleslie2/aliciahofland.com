0a2b5696966f19dacbef59ad1b5565cc68c2b368
Kyle Leslie <31903847+kyleslie2@users.noreply.github.com>
2020-03-03 09:03:57 -0500

    update

--- assets/script.js
+++ assets/script.js
@@ -25,23 +25,7 @@ function onClick(element) {
     captionText.innerHTML = element.alt;
 }
 
-$(function () {
-    console.log("ready!");
 
-    $("img").hover(function () {
-        // $(this).css("background-color", "yellow");
-        console.log('hovering over an image!')
-        var element_id = $(this).attr("id");
-        var id = '#'+element_id + "-overlay"
-        $(id).css("visibility","visible")
-    }, function () {
-      var element_id = $(this).attr("id");
-      var id = '#' + element_id + "-overlay"
-      $(id).css("visibility", "hidden")
-      });
-
-
-})
 
 
 
