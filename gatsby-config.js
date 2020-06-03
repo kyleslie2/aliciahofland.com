6e23d97210356a59b315e3cf5df2df4b92075dfb
Kyle Leslie <31903847+kyleslie2@users.noreply.github.com>
2020-06-03 08:41:31 -0400

    build test

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,13 +13,13 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `markdown-pages`,
-        path: `${__dirname}/src/markdown-pages`,
-      },
-    },
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `markdown-pages`,
+    //     path: `${__dirname}/src/markdown-pages`,
+    //   },
+    // },
      {
        resolve: `gatsby-transformer-remark`,
        options: {
