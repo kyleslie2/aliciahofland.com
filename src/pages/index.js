ecdef67f6a1dba8ff33f2db21c23deda4db153c6
Kyle Leslie <31903847+kyleslie2@users.noreply.github.com>
2020-06-02 22:07:35 -0400

    new site

---
+++ src/pages/index.js
@@ -0,0 +1,27 @@
+import React from "react"
+// import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+// import Image from "../components/image"
+import SEO from "../components/seo"
+import BackgroundImage from "../components/background-image"
+// import styled from "styled-components"
+
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <BackgroundImage fileName="background.png" />
+
+    
+  </Layout>
+)
+
+export default IndexPage
+
+
+
+
+
+
+
